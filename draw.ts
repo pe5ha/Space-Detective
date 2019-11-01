90fc38571b4be4f72e1ae166abcd7b0dd2131e23
Bagmutov <bagmutov94@mail.ru>
2019-11-02 00:27:52 +0300

    added export

--- draw.ts
+++ draw.ts
@@ -3,12 +3,14 @@
 class DRAW{
 
     static drawField(ctx:CanvasRenderingContext2D){
-        ctx.fillStyle="#000000";
-        ctx.fillRect(10,10,20,20);
-        ctx.fillRect(40,10,20,20);
-        ctx.fillRect(10,40,20,20);
-        ctx.fillRect(40,40,20,20);
-        // let f = Field.getField();
+        ctx.fillStyle="#aaadd";
+        // ctx.fillRect(10,10,20,20);
+        // ctx.fillRect(40,10,20,20);
+        // ctx.fillRect(10,40,20,20);
+        // ctx.fillRect(40,40,20,20);
+        let f = Field.getField();
     }
 
 }
+
+export{DRAW}
