9fe09381637842ef5e18fef8b50e0cfb3bc6c017
Ivan Sergeev <mr.ivandmi@yandex.ru>
2019-11-12 21:01:13 +0300

    added brain

---
+++ brain.ts
@@ -0,0 +1,31 @@
+
+class Brain{
+    
+    TypeOfResp: number; //means npc or item
+    NameOfNpc: string;      //its to hard to explain      
+    NumOfResp: number;  //heres num of Player choice
+
+
+    //here we read num of scene or num of dialog then we
+    // get num of player's answer and  look for the appropriate
+    // text
+    getDialogline(NumOfPlayerResp): string {
+        
+        //getting text 
+        //yet dont know how 
+        //here we use NameOfNpc and NumOfResp
+
+        return;
+
+    }
+
+    AskField(): number{
+        
+        //ask Field type of resp and name of npc
+        //then write its in TypeOfRespond NameOfNpc 
+        //and NumOfRespond
+
+        return ;
+
+    }
+}
