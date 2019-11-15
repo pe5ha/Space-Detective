db73447c73014a1207a53beacd297d0b88501867
Bagmutov <53879381+Bagmutov@users.noreply.github.com>
2019-11-16 00:13:58 +0300

    Update main.ts

--- main.ts
+++ main.ts
@@ -17,10 +17,10 @@ testc.height=500;
 initCanvas(canv);	
 
 // PARAMETERS:
-// let step = 1/2, n=3, ptr=.0, clr='#000000', speed=.1, size=10, fadingOn=false;//mystery shape
+ let step = 1/2, n=3, ptr=.0, clr='#000000', speed=.1, size=10, fadingOn=false;//mystery shape
 // let step = 1/2, n=6, ptr=.0, clr=getRndClr()+"77", speed=1000, size=1, fadingOn=false;//snowflower
 // let step = 1/3, n=3, ptr=.0, clr=getRndClr()+"77", speed=1000, size=1, fadingOn=true;//radioactive
-let step = 1/2, n=5, ptr=.0, clr=getRndClr()+"77", speed=1000, size=5, fadingOn=true;//
+//let step = 1/2, n=5, ptr=.0, clr=getRndClr()+"77", speed=1000, size=5, fadingOn=true;//
 
 let abc=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], xy={x:canv.width/2,y:canv.height/2},ast=Math.random()*6.29, speedsum=0;
 // abc.forEach((a,i,arr)=>{arr[i]=Math.random()*((i%2)?canv.height:canv.width)});
