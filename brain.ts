b25b3719e66b9f00e8c4565d14d3ab9157f0c5b8
Ivan Sergeev <mr.ivandmi@yandex.ru>
2019-11-15 20:47:19 +0300

    new textetxtetxtet

--- brain.ts
+++ brain.ts
@@ -1,15 +1,20 @@
+import { mainField } from "./main";
 
 class Brain{
     
-    TypeOfResp: number; //means npc or item
-    NameOfNpc: string;      //its to hard to explain      
-    NumOfResp: number;  //heres num of Player choice
+    // typeOfResp: number; //means npc or item
+    nameOfNpc: string;      //its to hard to explain      
+    // numOfResp: number;  //heres num of Player choice
+    mydial:Dialog;
 
+    constructor(name:string){
+        this.mydial = allDialogs[name];
+    }
 
     //here we read num of scene or num of dialog then we
     // get num of player's answer and  look for the appropriate
     // text
-    getDialogline(NumOfPlayerResp): string {
+    getDialogline(NumOfPlayerResp:string): string {
         
         //getting text 
         //yet dont know how 
@@ -28,4 +33,19 @@ class Brain{
         return ;
 
     }
+}
+
+type Reply = string;
+type Condition = ()=>boolean;
+type Dialog = {cond:Condition, repl:Reply}[];
+
+let allDialogs = {
+    "bob1":{
+        {cond:()=>{return mainField.h==20}; repl:"Field h is 20"},
+        {cond:()=>{return mainField.h==21}; repl:"Field h is 21"},
+        {cond:()=>{return true}; repl:"Field h is hh"}
+
+
+    },
+    "bob2":{}
 }
