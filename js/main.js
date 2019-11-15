7762be79a417ac732251fe0c8f210f780fb63900
Bagmutov <bagmutov94@mail.ru>
2019-11-15 20:27:50 +0300

    refined stuff

--- js/main.js
+++ js/main.js
@@ -13,10 +13,14 @@ testc.height = 500;
 // let abc=[50,0,450,0,500,250,250,500,0,250], xy={x:0,y:0};
 initCanvas(canv);
 // PARAMETERS:
-let step = 1 / 2, n = 6, ptr = .0, clr = getRndClr() + "77";
-let abc = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], xy = { x: 0, y: 0 }, ast = Math.random() * 6.29;
+// let step = 1/2, n=3, ptr=.0, clr='#000000', speed=.1, size=10, fadingOn=false;//mystery shape
+// let step = 1/2, n=6, ptr=.0, clr=getRndClr()+"77", speed=1000, size=1, fadingOn=false;//snowflower
+// let step = 1/3, n=3, ptr=.0, clr=getRndClr()+"77", speed=1000, size=1, fadingOn=true;//radioactive
+let step = 1 / 2, n = 5, ptr = .0, clr = getRndClr() + "77", speed = 1000, size = 5, fadingOn = true; //
+let abc = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], xy = { x: canv.width / 2, y: canv.height / 2 }, ast = Math.random() * 6.29, speedsum = 0;
 // abc.forEach((a,i,arr)=>{arr[i]=Math.random()*((i%2)?canv.height:canv.width)});
 abc.forEach((a, i, arr) => { arr[i] = ((i % 2) ? canv.height * (.5 + .5 * Math.sin(ast + ptr * Math.random() + 6.29 * (i - 1) / 2 / n)) : canv.width * (.5 + .5 * Math.cos(ast + ptr * Math.random() + 6.29 * (i) / 2 / n))); });
+let drawThingie = (size > 3) ? (x, y) => { drawCircle(mainctx, x, y, size / 2); } : (x, y) => { mainctx.fillRect(x, y, size, size); };
 initGame();
 //alert("SPACE DETECTIVE HELLOOOOO");    
 mainLoop();
@@ -26,6 +30,18 @@ function mainLoop() {
     allDraw();
     requestAnimationFrame(mainLoop);
 }
+export function drawCircle(ctx, x, y, r, clr = null, stroke = 0) {
+    if (clr)
+        ctx.fillStyle = ctx.strokeStyle = clr;
+    ctx.beginPath();
+    ctx.arc(x, y, r, 0, 6.29);
+    ctx.lineWidth = stroke;
+    if (stroke > 0)
+        ctx.stroke();
+    else
+        ctx.fill();
+}
+;
 function initGame() {
     DRAW.init(canv.width, canv.height);
     mainField = new Field(24, 10);
@@ -49,15 +65,19 @@ function allAction() {
 function allDraw() {
     // DRAW.drawField(mainctx);
     mainctx.fillStyle = clr;
-    for (let i = 0; i < 1000; i++) {
+    while (speedsum > 0) {
         makeStep(n);
+        speedsum--;
     }
-    // mainctx.fillStyle="#ffffff01";
-    // mainctx.fillRect(0,0,canv.width,canv.height);
+    speedsum += speed;
+    if (fadingOn) {
+        mainctx.fillStyle = "#ffffff01";
+        mainctx.fillRect(0, 0, canv.width, canv.height);
+    }
     function makeStep(n) {
         let r = ~~(Math.random() * n);
         xy = goto(xy, abc[r * 2], abc[r * 2 + 1], step);
-        mainctx.fillRect(xy.x, xy.y, 1, 1);
+        drawThingie(xy.x, xy.y);
     }
     function goto(xy, x, y, a = .5) {
         return { x: xy.x + (x - xy.x) * a, y: xy.y + (y - xy.y) * a };
