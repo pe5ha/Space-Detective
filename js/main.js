95277de1ccd21f84abfb2ac62fcf630eb991434b
Bagmutov <bagmutov94@mail.ru>
2019-11-16 22:09:34 +0300

    background color added

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-import { Field, Person, getRndClr } from "./field.js";
+import { Field, Person } from "./field.js";
 import { DRAW } from "./draw.js";
 let canv = document.getElementById('canvas');
 let mainctx = canv.getContext('2d');
@@ -13,14 +13,17 @@ testc.height = 500;
 // let abc=[50,0,450,0,500,250,250,500,0,250], xy={x:0,y:0};
 initCanvas(canv);
 // PARAMETERS:
-// let step = 1/2, n=3, ptr=.0, clr='#000000', speed=.1, size=10, fadingOn=false;//mystery shape
+let bgclr = "#000000";
+let step = 1 / 2, n = 3, ptr = .0, clr = '#ffffff', speed = .1, size = 10, fadingOn = false; //mystery shape
 // let step = 1/2, n=6, ptr=.0, clr=getRndClr()+"77", speed=1000, size=1, fadingOn=false;//snowflower
 // let step = 1/3, n=3, ptr=.0, clr=getRndClr()+"77", speed=1000, size=1, fadingOn=true;//radioactive
-let step = 1 / 2, n = 5, ptr = .0, clr = getRndClr() + "77", speed = 1000, size = 5, fadingOn = true; //
+// let step = 1/2, n=5, ptr=.0, clr=getRndClr()+"77", speed=1000, size=5, fadingOn=true;//
 let abc = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], xy = { x: canv.width / 2, y: canv.height / 2 }, ast = Math.random() * 6.29, speedsum = 0;
 // abc.forEach((a,i,arr)=>{arr[i]=Math.random()*((i%2)?canv.height:canv.width)});
 abc.forEach((a, i, arr) => { arr[i] = ((i % 2) ? canv.height * (.5 + .5 * Math.sin(ast + ptr * Math.random() + 6.29 * (i - 1) / 2 / n)) : canv.width * (.5 + .5 * Math.cos(ast + ptr * Math.random() + 6.29 * (i) / 2 / n))); });
 let drawThingie = (size > 3) ? (x, y) => { drawCircle(mainctx, x, y, size / 2); } : (x, y) => { mainctx.fillRect(x, y, size, size); };
+mainctx.fillStyle = bgclr;
+mainctx.fillRect(0, 0, canv.width, canv.height);
 initGame();
 //alert("SPACE DETECTIVE HELLOOOOO");    
 mainLoop();
@@ -71,7 +74,7 @@ function allDraw() {
     }
     speedsum += speed;
     if (fadingOn) {
-        mainctx.fillStyle = "#ffffff01";
+        mainctx.fillStyle = bgclr + "01";
         mainctx.fillRect(0, 0, canv.width, canv.height);
     }
     function makeStep(n) {
--- main.ts
+++ main.ts
@@ -17,10 +17,11 @@ testc.height=500;
 initCanvas(canv);	
 
 // PARAMETERS:
-// let step = 1/2, n=3, ptr=.0, clr='#000000', speed=.1, size=10, fadingOn=false;//mystery shape
+let bgclr="#000000";
+let step = 1/2, n=3, ptr=.0, clr='#ffffff', speed=.1, size=10, fadingOn=false;//mystery shape
 // let step = 1/2, n=6, ptr=.0, clr=getRndClr()+"77", speed=1000, size=1, fadingOn=false;//snowflower
 // let step = 1/3, n=3, ptr=.0, clr=getRndClr()+"77", speed=1000, size=1, fadingOn=true;//radioactive
-let step = 1/2, n=5, ptr=.0, clr=getRndClr()+"77", speed=1000, size=5, fadingOn=true;//
+// let step = 1/2, n=5, ptr=.0, clr=getRndClr()+"77", speed=1000, size=5, fadingOn=true;//
 
 let abc=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], xy={x:canv.width/2,y:canv.height/2},ast=Math.random()*6.29, speedsum=0;
 // abc.forEach((a,i,arr)=>{arr[i]=Math.random()*((i%2)?canv.height:canv.width)});
@@ -28,6 +29,8 @@ abc.forEach((a,i,arr)=>{arr[i]=((i%2)?canv.height*(.5+.5*Math.sin(ast+ptr*Math.r
 
 let drawThingie = (size>3)?(x,y)=>{drawCircle(mainctx,x,y,size/2);}:(x,y)=>{mainctx.fillRect(x,y,size,size);};
 
+mainctx.fillStyle=bgclr;
+mainctx.fillRect(0,0,canv.width,canv.height);
 initGame();
 //alert("SPACE DETECTIVE HELLOOOOO");    
 mainLoop();
@@ -83,7 +86,7 @@ function allDraw(){
 	}
 	speedsum+=speed;
 	if(fadingOn){
-		mainctx.fillStyle="#ffffff01";
+		mainctx.fillStyle=bgclr+"01";
 		mainctx.fillRect(0,0,canv.width,canv.height);
 	}
 
